7dc52d3f52bd790f3158d0eaeb76854ca91c72da
maxxvinycius <maxvinycius36@gmail.com>
2026-05-19 14:57:21 -0300

    Add files via upload

---
+++ menu/script.js
@@ -0,0 +1,45 @@
+const botoes = document.querySelectorAll("#card button");
+
+
+// ===============================
+// JOGO DA MEMÓRIA
+// ===============================
+botoes[0].addEventListener("click", () => {
+
+  window.location.href =
+    "../Jogo da Memória/index.html";
+
+});
+
+
+// ===============================
+// JOGO DA VELHA
+// ===============================
+botoes[1].addEventListener("click", () => {
+
+  window.location.href =
+    "../Jogo da Velha/index.html";
+
+});
+
+
+// ===============================
+// JOGO DE ADIVINHAÇÃO
+// ===============================
+botoes[2].addEventListener("click", () => {
+
+  window.location.href =
+    "../Jogo da Adivinhação/index.html";
+
+});
+
+
+// ===============================
+// PEDRA PAPEL TESOURA
+// ===============================
+botoes[3].addEventListener("click", () => {
+
+  window.location.href =
+    "../Pedra, Papel, tesoura/index.html";
+
+});
